b18adea1f60d35fe9ede1880dd36821ea2a77b98
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-24 01:48:42 +0100

    Resume tweaking

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const header = document.querySelector('header');
+const navigation = document.querySelector('.navigation');
+const hamburger = document.querySelector('.hamburger');
 
 window.addEventListener('scroll', () => {
 	header.classList.toggle('fixed', window.scrollY > 0);
+});
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	navigation.classList.toggle('active');
+});
+
+window.addEventListener('click', (event) => {
+	if (
+		event.target.nodeName === 'A' &&
+		navigation.classList.contains('active')
+	) {
+		hamburger.classList.toggle('active');
+		navigation.classList.toggle('active');
+	}
 });
